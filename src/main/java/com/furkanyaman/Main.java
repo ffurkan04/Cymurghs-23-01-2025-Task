b9b7cf3ff526cdaaf2e19dad02d6831b4122112a
Furkan Yaman <furkiyyy24@gmail.com>
2025-01-22 23:25:48 +0300

    Ters Piramit yapmak

--- src/main/java/com/furkanyaman/Main.java
+++ src/main/java/com/furkanyaman/Main.java
@@ -15,21 +15,44 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
         int lineNumber = keyboard.nextInt();
         
-        for(int line = 0; line <lineNumber; line++){
-/*yukarıdaki şekle göre satır arttıkça boşluk azalıyor. 
-Bunu da girilen satır sayısı - döngünün bulunduğu satır olarak hesaplanabiliyor.
-*/
-            for(int space=0 ; space<lineNumber-(line);space++){
-                System.out.print(" ");
-            }
-//ln.7-13 yorum satırındaki şemaya göre her satırda satır numarasının 2 katının 1 fazlası yıldız var. 
-//yıldız sayısı bunu geçmemesi gerektiği için ln.23 de küçüktür işareti kullanıyoruz.
-//Örn. ln. 1 için 1 +(1*2) = 3 olacak şekilde 3 kez çalışır ve 3 tane * koyar. 
-            for(int star= 0; star<1+(line*2); star++){ 
-                System.out.print("*");
-            }
-            System.out.println();
+        
+         for(int line = 0; line <lineNumber; line++){
+ /*yukarıdaki şekle göre satır arttıkça boşluk azalıyor. 
+ Bunu da girilen satır sayısı - döngünün bulunduğu satır olarak hesaplanabiliyor.
+ */
+             for(int space=0 ; space<lineNumber-(line);space++){
+                 System.out.print(" ");
+             }
+ /*ln.7-13 yorum satırındaki şemaya göre her satırda satır numarasının 2 katının 1 fazlası yıldız var. 
+ yıldız sayısı bunu geçmemesi gerektiği için ln.23 de küçüktür işareti kullanıyoruz.
+ Örn. ln. 1 için 1 +(1*2) = 3 olacak şekilde 3 kez çalışır ve 3 tane * koyar. */
+             for(int star= 0; star<1+(line*2); star++){ 
+                 System.out.print("*");
+             }
+             System.out.println();
 
+         }
+
+
+        //Ters piramit
+
+        /*
+          ********* 0   Satır numarası = 0 yapıp arttırırsak düz piramit
+           *******  1   Satır numarası =istenilen satır yapıp azaltırsak ters piramit olur.
+            *****   2   
+             ***    3   Boşluk= istenilen satır - satır numarası
+              *     4
+         */
+        //Not terste satır numarası yaparken istenilen satırı -1 yapmazsan döngü 1 defa fazla çalışır. 
+
+       for(int line = lineNumber-1; line>=0; line--){
+        for(int space= 0; space<lineNumber-line; space++){
+            System.out.print(" ");
         }
+        for(int star = 0; star<1+(line*2); star++){
+            System.out.print("*");
+        }
+        System.out.println(); // satır atlamak için
+       }
     }
 }
